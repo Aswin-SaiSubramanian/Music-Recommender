f9ef5cb5872dd263b255243f3ad5ac0858a848c7
MphiGithub <matthew.mphi@gmail.com>
2021-04-10 14:28:13 -0700

    Fixed latin subset

--- AssembleDataset_LatinElectronic/genre_filter.py
+++ AssembleDataset_LatinElectronic/genre_filter.py
@@ -1,10 +1,10 @@
 import os
 # Replace the path below with the path of id list
-names_file = open('/home/matthew/Desktop/Latin')
+names_file = open('/home/matthew/Desktop/CPEN291_Final_Project_Repo/project-team-dilophosaurus/AssembleDataset_LatinElectronic/Latin')
 names = set(line.rstrip('\n') for line in names_file.readlines())
 names_file.close()
 # Replace with path of MillionSongDataset (10000 Samples)
-for root, dirs, files in os.walk('/home/matthew/Desktop/MillionSongSubset'):
+for root, dirs, files in os.walk('/home/matthew/Desktop/CPEN291_Final_Project_Repo/project-team-dilophosaurus/Latin Subset'):
     for name in files:
         path = os.path.join(root, name)
         if os.path.isfile(path):
