5fe031f35c20ba0b51468cc289602d47bed827d0
Aswin-SaiSubramanian <aswin.sai009@gmail.com>
2021-05-11 19:15:03 -0700

    Changed default window size

--- song_recommender.py
+++ song_recommender.py
@@ -2,7 +2,8 @@ import matplotlib
 matplotlib.use('Agg')
 
 import tkinter as tk
-
+from tkinter import filedialog
+#import recommender as rec
 
 class Application(tk.Frame):
     def __init__(self, master = None):
@@ -10,22 +11,52 @@ class Application(tk.Frame):
             self.master = master
             self.pack()
             self.create_widgets()
-
+            
+    def enable(x):
+        x.config(state=NORMAL)
+        
+    def disable(x):
+        x.config(state=DISABLED)
+    
+    
     def create_widgets(self):
             self.recommender = tk.Button(self)
             self.recommender["text"] = "Get Recommendation"
             self.recommender["command"] = self.run_recommender
-            self.recommender.pack(side="bottom")
+            self.recommender.pack(side="top")
+
+            self.browse = tk.Button(self)
+            self.browse["text"] = "Browse..."
+            self.browse["command"] = self.file_browser
+            self.recommender.pack(side="right")
 
             self.quit = tk.Button(self, text="EXIT", fg="red", command=self.master.destroy)
             self.quit.pack(side="bottom")
-    
+
+            # self.disable()
+
+    def file_browser(self):
+            file = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a file')
+            if file:
+                data = file.read()
+                file.close()
+                print("Success") # for testing
+                # self.enable(self.recommender)
+            else:
+                # self.disable(self.recommender)
+                print("File not available.")
+
     def run_recommender(self):
-        # print("Pass")
-        pass
+            if file:
+                # Run the recommender
+                print("")
+            else:
+                print("Please select an audio file.")
+        
 
 
 root = tk.Tk()
+root.geometry("600x400")
 app = Application(master = root)
 app.mainloop()
 
