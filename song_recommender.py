043632b6c640a8db3e63ffcbcb59e5011819cc22
Aswin-SaiSubramanian <aswin.sai009@gmail.com>
2021-05-15 20:54:01 -0700

    Updated the GUI's prompts, made GUI robust to invalid input file extensions

--- song_recommender.py
+++ song_recommender.py
@@ -1,6 +1,5 @@
 import matplotlib
-matplotlib.use('Agg')
-
+matplotlib.use('Agg') 
 import tkinter as tk
 from tkinter import filedialog
 from recommender import Recommender as rec
@@ -28,7 +27,7 @@ class Application(tk.Frame):
             # don't assume that self.parent is a root window.
             # instead, call `winfo_toplevel to get the root window
             # (https://stackoverflow.com/questions/2395431/using-tkinter-in-python-to-edit-the-title-bar)
-            self.winfo_toplevel().title("Music Recommender")
+            self.winfo_toplevel().title("Parasaurolophus: Music Recommender")
 
             # Adding GUI components...
             
@@ -42,6 +41,7 @@ class Application(tk.Frame):
             # ... a text box in which the application displays its outputs instead of command line
             self.text = tk.Text(self.master)
             self.text.pack(side=tk.BOTTOM, pady = 5, padx = 5)
+            self.text.insert(tk.INSERT, "Welcome to the parasaurolophus' home! \nPlease show me an example of a song you like (wav file, please).") 
             self.disable(self.text)
 
             # ... a button to tell the app to open a file browser (for selecting a wav file as the app's input)
@@ -52,7 +52,7 @@ class Application(tk.Frame):
 
             # ... a button to initiate the song recommendation process
             self.recommender = tk.Button(self)
-            self.recommender["text"] = "Get Recommendation"         # setting display text
+            self.recommender["text"] = "Get Recommendation!"         # setting display text
             self.recommender["command"] = self.run_recommender      # setting button's function
             self.recommender.pack(side=tk.TOP, pady = VERT_PAD, padx = HOR_PAD)
             
@@ -60,9 +60,10 @@ class Application(tk.Frame):
             
     def file_browser(self):
             self.file = tk.filedialog.askopenfile(parent=root,mode='rb',title='Choose a file')
-            if self.file:
+            if self.file and self.file.name.split('.')[-1] == "wav":
                 self.status = True                                  # Recording that an input wav file has been selected
                 self.name_of_target_file = self.file.name           # Gets the path to the selected wav file
+                # print(self.name_of_target_file.split('.')[-1])
                 self.name_of_target_file = self.name_of_target_file.split('/')[-1]  # Extracts the name of the wav file from its path 
                 self.file.close()
                 
@@ -74,14 +75,20 @@ class Application(tk.Frame):
                 
                 self.enable(self.recommender)
                 self.disable(self.browse)                           # Have already selected an input file. Only need the "Get recommendation" button.
-
+            
             else:
                 self.status = False                                 # Ensures that self.status remains False when no input wav file is selected
                 self.disable(self.recommender)
                 
                 # Writing feedback text to GUI
                 self.enable(self.text)
-                self.text.insert(tk.INSERT, "No file selected.") 
+                if self.file:
+                    if (self.file.name.split('.')[-1] != "wav"):
+                        self.text.delete('1.0', tk.END)
+                        self.text.insert(tk.INSERT, "Invalid file extension. Please select a wav file.") 
+                else:
+                    self.text.delete('1.0', tk.END)
+                    self.text.insert(tk.INSERT, "No file selected. Please select a wav file.") 
                 self.disable(self.text)
 
     def run_recommender(self):
@@ -96,7 +103,7 @@ class Application(tk.Frame):
 
                 # Writing music recommendation to GUI
                 self.enable(self.text)
-                disp_string = "You might also like: " + result
+                disp_string = "Parasaurolophus recommends: " + result
                 self.text.delete('1.0', tk.END)
                 self.text.insert(tk.INSERT, disp_string) 
                 # copy recommendation to clipboard (referred to https://stackoverflow.com/questions/579687/how-do-i-copy-a-string-to-the-clipboard)
